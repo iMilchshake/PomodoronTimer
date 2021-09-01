9355c16d43852416838b7589d791c28781edb21e
Tobias Schneider <tobias.schneider@smail.inf.h-brs.de>
2021-09-01 16:40:13 +0200

    added logger to backend

--- server_node/express_backend.js
+++ server_node/express_backend.js
@@ -1,6 +1,14 @@
 const express = require('express')
+// const expressWinston = require('express-winston');
+// const winston = require('winston'); // for transports.Console
+
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+
 const storage = require('./storage_cached')
 const statistics = require('./statistics')
+
 const app = express()
 const port = 3000
 
@@ -9,6 +17,35 @@ app.use(express.json());
 
 // initialize storage
 // storage.initialize(); should now be implicated by import
+// const tsFormat = () => (new Date()).toLocaleTimeString();
+// const tsFormat = () => (new Date()).getTimezoneOffset();
+// const myFormat = winston.format.printf(({ level, message, timestamp }) => {
+//     return `${timestamp} [${level}]: ${message}`;
+// });
+
+// app.use(expressWinston.logger({
+// //     transports: [
+// //         new (winston.transports.Console)({
+// //             timestamp: tsFormat,
+// //             colorize: true,
+// //             level: 'info'
+// //         }),
+// //         new (winston.transports.File)({
+// //             filename: 'somefile.log',
+// //             level: 'error'
+// //         })
+// //     ],
+// //     format: winston.format.combine(
+// //         winston.format.label({ label: 'right meow!' }),
+// //         winston.format.timestamp(),
+// //         myFormat
+// //     ),
+// // }));
+
+// setup the logger
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}))
+app.use(morgan('combined'))
 
 // setup routes
 app.get('/PomodoronTimer/api/data', (req, res) => {
@@ -38,7 +75,6 @@ app.get('/PomodoronTimer/api/data/timeSpend', (req, res) => {
         res.send(stats);
     })
 });
-
 
 // invalid route
 app.use(function (req, res) {
